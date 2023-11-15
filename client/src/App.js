b757cc511b10ec76baae666c24f74209185b80f1
Valeria Terziyska <valeria.terziyska@gmail.com>
2023-11-15 15:56:36 +0200

    components, services and utils

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+import './App.css';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { UserList } from './components/UserList';
+
+import * as userService from './services/userService';
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getAll()
+            .then(users => {
+                setUsers(users);
+            })
+            .catch(err => {
+                console.log(`Error` + err);
+            })
+    }, []);
+
+    return (
+        <>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+
+                    <Search />
+                    <UserList users={users} />
+                </section>
+            </main>
+
+            <Footer />
+        </>
+    );
+}
+
+export default App;
